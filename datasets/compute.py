79f7d79013846a65e6947c66b4b5bbb83e0e50a5
Austin Wallace <awallace43@gatech.edu>
2025-03-05 16:33:03 -0500

    compute

--- datasets/compute.py
+++ datasets/compute.py
@@ -3,6 +3,7 @@ import argparse
 import pandas as pd
 from tqdm import tqdm
 from qcportal import PortalClient
+import numpy as np
 
 # from qcportal.manybody import ManybodyDataset, ManybodyDatasetEntry, ManybodySpecification, ManybodyKeywords, BSSECorrectionEnum
 from qcportal.singlepoint import SinglepointDataset, SinglepointDatasetEntry
@@ -17,14 +18,13 @@ print(client)
 ROOT = os.getcwd()
 
 # df['qcel'] are qcelemental monomer Molecule objects
-df = pd.read_pickle("../data_dir/raw/monomers_ap3_spec_1.pkl")
 # df.head().to_pickle("./data_dir/raw/monomers_ap3_spec_1_head.pkl")
 # df = pd.read_pickle("../data_dir/raw/monomers_ap3_spec_1_head.pkl")
-print(df.columns.values)
+# print(df.columns.values)
 
 
 def add_QCdataset(
-    df: pd.DataFrame = df,
+    df: pd.DataFrame,
     system: str = "AP2-monomers",
     method: str = "PBE0",
     basis: str = "aug-cc-pVTZ",
@@ -98,40 +98,90 @@ def submit(ds: SinglepointDataset, tag: str = "short") -> None:
 def main():
     # mode = "create"
     # mode = "run"
+    mode = "progess"
     mode = "analyze"
     system = "AP2-monomers"
     method = "PBE0"
     basis = "aug-cc-pVTZ"
+
+    print(f"Running in {mode} mode")
+
+    df = pd.read_pickle("../data_dir/raw/monomers_ap3_spec_1.pkl")
     ds = add_QCdataset(df, system, method=method, basis=basis, mode=mode)
     if mode == "create" or mode == "run":
         if mode == "run":
             submit(ds, tag="normal")
+    elif mode == "progess":
+        ds.status()
+        ds.print_status()
     else:
         ds.status()
         ds.print_status()
-        # ds.list_specifications()
-        # print(
-        #     f"{len([0 for i in ds.iterate_records(status='complete')])} / {len(df)} comp. complete"
-        # )
         # return
-        # for entry_name, spec_name, record in tqdm(
-        #     ds.iterate_records(status="complete")
-        # ):
-        #     print(entry_name, spec_name)
-        #     mols = client.get_molecules(record.molecule_id)
-        #     # print(mols.to_string(dtype="xyz"))
-        #     record_dict = record.dict()
-        #     qcvars = record_dict["properties"]
-        #     charges = qcvars["mbis charges"]
-        #     dipoles = qcvars["mbis dipoles"]
-        #     quadrupoles = qcvars["mbis quadrupoles"]
-        #     level_of_theory = f"{record_dict['specification']['method']}/{record_dict['specification']['basis']}"
-        #     # pp(record_dict.keys())
-        #     # pp(qcvars.keys())
-        #     # pp(qcvars)
-        #     break
+        cnt = 0
+        data = {
+            "id": [],
+            "name": [],
+            "Z": [],
+            "R": [],
+            "cartesian_multipoles": [],
+            "entry_name": [],
+            "spec_name": [],
+            "TQ": [],
+            "molecular_multiplicity": [],
+            "volume ratios": [],
+            "valence widths": [],
+            "radial moments <r^2>": [],
+            "radial moments <r^3>": [],
+            "radial moments <r^4>": [],
+        }
+        for entry_name, spec_name, record in tqdm(
+            ds.iterate_records(status="complete")
+        ):
+            record_dict = record.dict()
+            qcvars = record_dict["properties"]
+            charges = qcvars["mbis charges"]
+            dipoles = qcvars["mbis dipoles"]
+            quadrupoles = qcvars["mbis quadrupoles"]
+            level_of_theory = f"{record_dict['specification']['method']}/{record_dict['specification']['basis']}"
+
+            n = len(charges)
+
+            charges = np.reshape(charges, (n, 1))
+            dipoles = np.reshape(dipoles, (n, 3))
+            quad = np.reshape(quadrupoles, (n, 3, 3))
+
+            quad = [q[np.triu_indices(3)] for q in quad]
+            quadrupoles = np.array(quad)
+            multipoles = np.concatenate(
+                [charges, dipoles, quadrupoles], axis=1)
+
+            data['volume ratios'].append(qcvars['mbis volume ratios'])
+            data['valence widths'].append(qcvars['mbis valence widths'])
+            data['radial moments <r^2>'].append(qcvars['mbis radial moments <r^2>'])
+            data['radial moments <r^3>'].append(qcvars['mbis radial moments <r^3>'])
+            data['radial moments <r^4>'].append(qcvars['mbis radial moments <r^4>'])
+            data["id"].append(record.molecule.extras['idx'])
+            data["name"].append(record.molecule.extras['name'])
+            data["Z"].append(record.molecule.atomic_numbers)
+            data["R"].append(record.molecule.geometry * qcel.constants.bohr2angstroms)
+            data["cartesian_multipoles"].append(multipoles)
+            data["entry_name"].append(entry_name)
+            data["spec_name"].append(spec_name)
+            data["TQ"].append(int(record.molecule.molecular_charge))
+            data["molecular_multiplicity"].append(
+                record.molecule.molecular_multiplicity
+            )
+            # print(qcvars.keys())
+            cnt += 1
+        df2 = pd.DataFrame(data)
+        print(df2)
+        df2.to_pickle(f"../data_dir/raw/monomers_ap3_spec_1_pbe0.pkl")
+            # break
         # df2 = ds.get_properties_df(['mbis charges', 'mbis dipoles', 'mbis quadrupoles', 'mbis radial moments <r^2>', 'mbis radial moments <r^3>', 'mbis radial moments <r^4>', 'mbis valence widths', 'mbis volume ratios', 'mbis free atom c volume', 'mbis free atom h volume', 'mbis free atom n volume', 'mbis free atom s volume',])
-        # print(df2)
+        # df2 = ds.get_properties_df(['molecule_id'])
+        print(df2)
+        print(df2.columns.values)
 
 
 if __name__ == "__main__":
