ceb1fc719e50742470cb594c9fe318853ec1d632
AMW Arch <awallace43@gatech.edu>
2025-05-29 14:51:23 -0400

    am hirshfeld predict_qcel_mols

--- tests/test_classical_components.py
+++ tests/test_classical_components.py
@@ -1,6 +1,7 @@
 import pytest
 import apnet_pt
 import qcelemental as qcel
+import os
 
 lr_water_dimer = qcel.models.Molecule.from_data("""
 0 1
@@ -17,6 +18,7 @@ H                     8.792148880000    -0.879960520000    -1.416549430000
 units bohr
 """)
 
+file_dir = os.path.dirname(os.path.abspath(__file__))
 
 def test_elst_multipoles():
     atom_model = apnet_pt.AtomModels.ap2_atom_model.AtomModel(
@@ -45,5 +47,35 @@ def test_elst_multipoles():
     return
 
 
+def test_elst_multipoles_am_hirshfeld():
+    atom_model = apnet_pt.AtomModels.ap3_atom_model.AtomHirshfeldModel(
+        ds_root=None,
+        ignore_database_null=True,
+        use_GPU=False,
+    )
+    atom_model.set_pretrained_model(file_dir + "/../models/am_hf_ensemble/am_0.pt")
+    print(atom_model)
+    monA = lr_water_dimer.get_fragment(0).copy()
+    monB = lr_water_dimer.get_fragment(1).copy()
+    multipoles = atom_model.predict_qcel_mols([monA, monB, monA.copy(), monB.copy()], batch_size=3)
+    assert len(multipoles) == 4, f"Expected 4 multipoles, got {len(multipoles)}"
+    mtp_A = multipoles[0]
+    mtp_B = multipoles[1]
+    E_elst = apnet_pt.multipole.eval_qcel_dimer(
+        mol_dimer=lr_water_dimer,
+        qA=mtp_A[0].numpy(),
+        muA=mtp_A[1].numpy(),
+        thetaA=mtp_A[2].numpy(),
+        qB=mtp_B[0].numpy(),
+        muB=mtp_B[1].numpy(),
+        thetaB=mtp_B[2].numpy(),
+    )
+    print(f"E_elst = {E_elst:.6f} kcal/mol")
+    E_ref = -0.7430384309295008
+    assert abs(E_elst - E_ref) < 1e-6, f"Expected {E_ref}, got {E_elst}"
+    return
+
+
 if __name__ == "__main__":
-    test_elst_multipoles()
+    # test_elst_multipoles()
+    test_elst_multipoles_am_hirshfeld()
