74475cf1b6bdd5893cb2e004712bfb8b3cd2dc12
Elizabeth Singh <esingh41@login-phoenix-rh9-2.pace.gatech.edu>
2025-08-25 22:34:30 -0400

    logic for training multiple atom models with different random seeds

--- sbatch_ensemble.py
+++ sbatch_ensemble.py
@@ -2,26 +2,27 @@ import os
 
 # t2 uses ap2 original ensemble for training, but on full dataset...
 
-def create_sbatch_ap2(submit=False):
+def create_sbatch_am_non_eq(submit=False):
     for i in range(0, 5):
-        fn = f"train_ap{i}.sbatch"
-        #os.system(f"cp -r data_dir_ex data_{i}")
+        fn = f"train_am_non_eq_cuda_{i}.sbatch"
+        #os.system(f"cp -r data_dir data_{i}")
         python_call = """python3 -u ./train_models.py \\
     --train_am "AtomModel" \\
-    --am_model_path ./models/am_neq/am_ensemble/am_$iter.pt \\
+    --am_model_path ./models/am_neq/am_ensemble_cuda/am_$iter.pt \\
     --spec_type_am 7 \\
     --random_seed $iter \\
     --n_epochs 500 \\
-    --lr 5e-5
+    --lr 5e-5 \\
     --data_dir ./data_$iter \\
         """
         with open(fn, 'w') as f:
             f.write(f"""#!/bin/bash
 #SBATCH -J AM_non_eq_model_{i}
-#SBATCH -o AM_non_eq_model_{i}_training.out
+#SBATCH -o AM_non_eq_model_{i}_cuda_training.out
 #SBATCH -Agts-cs207-chemx
 #SBATCH --open-mode=append
 #SBATCH -N1 --ntasks=1 --cpus-per-task=8 -G1
+#SBATCH --gres=gpu:1
 #SBATCH --mem-per-cpu=12G
 #SBATCH -t72:00:00
 #SBATCH -pgpu-a100
@@ -29,9 +30,10 @@ def create_sbatch_ap2(submit=False):
 #SBATCH --mail-user=esingh41@gatech.edu
 
 
-cd /storage/home/hcoda1/3/awallace43/gits/qcmlforge/
-source /storage/home/hcoda1/3/awallace43/p-cs207-0/miniconda/etc/profile.d/conda.sh
-conda activate /storage/home/hcoda1/3/awallace43/p-cs207-0/miniconda/envs/qcml
+cd /storage/home/hcoda1/1/esingh41/gits/QCMLForge_es
+module load cuda/12.6.1
+source /storage/home/hcoda1/1/esingh41/p-cs207-0/miniconda3/etc/profile.d/conda.sh
+conda activate /storage/home/hcoda1/1/esingh41/p-cs207-0/miniconda3/envs/qcml
 
 iter={i}
 echo "
@@ -43,3 +45,5 @@ echo "
     # --lr_decay 0.1 \\
         if submit:
             os.system(f'sbatch {fn}')
+
+create_sbatch_am_non_eq()
